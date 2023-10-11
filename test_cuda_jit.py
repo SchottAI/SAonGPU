5e56bdb934943718c22c3f524464aa3edf9ecb18
Lucas Kletzander <lucas.kletzander@gmail.com>
2023-10-11 12:19:19 +0200

    Update for test of cuda.jit

--- test_cuda_jit.py
+++ test_cuda_jit.py
@@ -4,28 +4,35 @@ import numpy as np
 from timeit import default_timer as timer
 
 
-def func(a, n):
+@jit
+def func(a, n, nr):
 
     for i in range(n):
-        a[i] += 1
+        for j in range(nr):
+            a[i] += 1
 
 
 @cuda.jit
-def func2(a):
+def func2(a, i):
 
     tx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
-    a[tx] += 1
+    for j in range(i):
+        a[tx] += 1
 
 
 if __name__ == "__main__":
-    n = 100000000
+    n = 10000000
 
-    start = timer()
-    a = np.random.rand(n)
-    func(a, n)
-    print("without GPU:", timer() - start, max(a))
+    nr = 1
+    for i in range(10):
 
-    start = timer()
-    a = np.random.rand(n)
-    func2[n // 1000, 1000](a)
-    print("with GPU:", timer() - start, max(a))
+        start = timer()
+        a = np.random.rand(n)
+        func(a, n, nr)
+        print("without GPU:", nr, timer() - start, max(a))
+
+        start = timer()
+        a = np.random.rand(n)
+        func2[n // 1000, 1000](a, nr)
+        print("with GPU:", nr, timer() - start, max(a))
+        nr *= 10
